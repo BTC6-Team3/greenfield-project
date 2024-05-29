6b295be3c11531840941ab6a1c1ba5392fbf9fc7
Kohichi-Isogai <143569429+Kohichi-Isogai@users.noreply.github.com>
2024-05-29 17:54:28 +0900

    users データベース作成

---
+++ backend/db/knexfile.js
@@ -0,0 +1,27 @@
+// Update with your config settings.
+require("dotenv").config();
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DB_NAME,
+
+    migrations: {
+      tableName: "./migrations",
+    },
+  },
+};
