769b88045d1577997d3e1e8259c1bfa08845bc78
いっちー <160448478+Ichihara-Soma@users.noreply.github.com>
2024-05-29 18:40:02 +0900

    input コンポーネント作成
    Co-authored-by: Takuma Suzuki <TakumaSuzuki1472192@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { Routes, Route } from "react-router-dom";
 import { SignupPage } from "./pages/SignupPage/SignupPage";
+import { InputPage } from "./pages/InputPage";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
       <MantineProvider>
         <Routes>
           <Route path="signup" element={<SignupPage />} />
+          <Route path="input" element={<InputPage />} />
         </Routes>
       </MantineProvider>
     </>
