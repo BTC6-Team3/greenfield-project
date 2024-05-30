938c1f6cb84a9364b55e767ba3d62dffaa7484a3
takumaBTC6 <takuma_suzuki_ab@mail.toytota.co.jp>
2024-05-30 14:27:35 +0900

    fix : deployのために設定追加

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,12 @@ const knex = require("knex")(knexConfig[environment]);
 const rootRouter = require("./route/root");
 // const signInRouter = require("./route/signIn");
 const signUpRouter = require("./route/signUp");
+
 const createServer = () => {
   const app = express();
   app.use(express.json());
   app.use(cors());
+  app.use(express.static(__dirname + "../../frontend/dist"));
   // app.use("/signIn", signInRouter());
   app.use("/signUp", signUpRouter(knex));
   // app.use("/", rootRouter());
