cf8cfe5979b3b267ba228315fb8efaf3903caa1b
tmc-kitagawa <masahiro_kitagawa@mail.toyota.co.jp>
2024-05-31 14:27:26 +0900

    fix: path for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const knex = require("./config/knex");
-const signInRouter = require("./route/signIn");
-const signUpRouter = require("./route/signUp");
+const express = require('express');
+const cors = require('cors');
+const knex = require('./config/knex');
+const signInRouter = require('./route/signIn');
+const signUpRouter = require('./route/signUp');
+const path = require('path');
 
 const createServer = () => {
   const app = express();
   app.use(express.json());
   app.use(cors());
-  app.use(express.static(path.join(__dirname, "../../frontend", "dist")));
-  app.use("/signIn", signInRouter());
-  app.use("/signUp", signUpRouter(knex));
+  app.use(express.static(path.join(__dirname, '../../frontend', 'dist')));
+  app.use('/signIn', signInRouter());
+  app.use('/signUp', signUpRouter(knex));
   app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, '../../frontend', 'dist', 'index.html'));
   });
 
   return app;
