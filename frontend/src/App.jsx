9cb66778a6d232c88b26bddd85db5a139861c3f8
tmc-kitagawa <masahiro_kitagawa@mail.toyota.co.jp>
2024-05-31 16:02:42 +0900

    add: drive_route_again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { Routes, Route } from "react-router-dom";
-import { InputPage } from "./pages/InputPage";
-import { SignupPage } from "./pages/SignupPage";
-import { SigninPage } from "./pages/SigninPage";
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { Routes, Route } from 'react-router-dom';
+import { InputPage } from './pages/InputPage';
+import { SignupPage } from './pages/SignupPage';
+import { SigninPage } from './pages/SigninPage';
+import { DriveRoutePage } from './pages/DriveRoutePage';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="signup" element={<SignupPage />} />
           <Route path="input" element={<InputPage />} />
           <Route path="signin" element={<SigninPage />} />
+          <Route path="drive_route" element={<DriveRoutePage />} />
           <Route path="/" element={<SignupPage />} />
         </Routes>
       </MantineProvider>
