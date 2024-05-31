e0a7254f1cfa95747eb4a5a586ebde8b1f704423
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-05-31 17:39:43 +0900

    add : 入力画面の作成完了
    Co-authored-by: HirokiObata tmc-kitagawa <hirokiobata tmc-kitagawa@users.noreply.github.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
-import { Routes, Route } from 'react-router-dom';
-import { InputPage } from './pages/InputPage';
-import { SignupPage } from './pages/SignupPage';
-import { SigninPage } from './pages/SigninPage';
-import { DriveRoutePage } from './pages/DriveRoutePage';
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+import { Routes, Route } from "react-router-dom";
+import { InputPage } from "./pages/InputPage";
+import { SignupPage } from "./pages/SignupPage";
+import { SigninPage } from "./pages/SigninPage";
+import { DriveRoutePage } from "./pages/DriveRoutePage";
+import { SelectSpotPage } from "./pages/SelectSpotPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="input" element={<InputPage />} />
           <Route path="signin" element={<SigninPage />} />
           <Route path="drive_route" element={<DriveRoutePage />} />
+          <Route path="select_spot" element={<SelectSpotPage />} />
           <Route path="/" element={<SignupPage />} />
         </Routes>
       </MantineProvider>
