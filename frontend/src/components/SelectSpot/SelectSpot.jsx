df6c55ac28cefb358d805335f77da7dfa7d4e520
tmc-kitagawa <masahiro_kitagawa@mail.toyota.co.jp>
2024-06-03 16:02:46 +0900

    add: map fundemental
    Co-authored-by: HirokiObata Sakaguchi123 <hirokiobata sakaguchi123@users.noreply.github.com>

--- frontend/src/components/SelectSpot/SelectSpot.jsx
+++ frontend/src/components/SelectSpot/SelectSpot.jsx
@@ -1,8 +1,8 @@
-import { Button, Center, Checkbox, Flex } from "@mantine/core";
-import { useForm } from "@mantine/form";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Button, Center, Checkbox, Flex } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export const SelectSpot = () => {
   const [spots, setSpots] = useState([]);
@@ -14,23 +14,25 @@ export const SelectSpot = () => {
   const areaName = location.state.areaName;
 
   const form = useForm({
-    mode: "uncontrolled",
+    mode: 'uncontrolled',
     initialValues: {
       select: false,
     },
 
     validate: {
-      select: value => (value === "" ? "エリアを選択してください。" : null),
+      select: (value) => (value === '' ? 'エリアを選択してください。' : null),
     },
   });
 
   const submit = () => {
-    navigate("/drive_route", { state: { spotId: checkedSpot } });
+    navigate('/drive_route', { state: { spotLocation: checkedSpot } });
   };
 
   useEffect(() => {
     (async () => {
-      const spotList = await axios(`/api/spots/${areaId}`).then(res => res.data);
+      const spotList = await axios(`/api/spots/${areaId}`).then(
+        (res) => res.data
+      );
       setSpots(spotList);
       // console.log(spotList);
     })();
@@ -39,11 +41,22 @@ export const SelectSpot = () => {
     <>
       <h1>{areaName}</h1>
       <Center>
-        <Flex mih={50} gap="md" justify="center" align="left" direction="column" wrap="wrap">
+        <Flex
+          mih={50}
+          gap="md"
+          justify="center"
+          align="left"
+          direction="column"
+          wrap="wrap"
+        >
           <form onSubmit={form.onSubmit(submit)}>
             <Checkbox.Group value={checkedSpot} onChange={setCheckedSpot}>
-              {spots.map(obj => (
-                <Checkbox label={obj.name} value={JSON.stringify(obj)} key={obj.tourist_spot_id} />
+              {spots.map((obj) => (
+                <Checkbox
+                  label={obj.name}
+                  value={JSON.stringify(obj)}
+                  key={obj.tourist_spot_id}
+                />
               ))}
             </Checkbox.Group>
             <Button variant="filled" color="gray" radius="xl" type="submit">
