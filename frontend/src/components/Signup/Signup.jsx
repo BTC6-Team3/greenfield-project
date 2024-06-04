3a3857039eed322222859cec3dc8cf1d6d2fcc58
Sakaguchi123 <chiyumi_sakaguchi@mail.toyota.co.jp>
2024-06-04 09:30:09 +0900

    add : login画面作成
    Co-authored-by: HirokiObata <hirokiobata@users.noreply.github.com>

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Button, Group, TextInput } from "@mantine/core";
+import { Button, Group, Stack, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 
 export const Signup = () => {
@@ -14,46 +14,47 @@ export const Signup = () => {
     },
 
     validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
-      name: (value) => (value.length < 1 ? "名前を入力してください。" : null),
-      password: (value) =>
-        value.length < 6 ? "パスワードは6文字以上で入力してください。" : null,
+      email: value => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      name: value => (value.length < 1 ? "名前を入力してください。" : null),
+      password: value => (value.length < 6 ? "パスワードは6文字以上で入力してください。" : null),
     },
   });
 
-  const handleOnSubmit = async (values) => {
+  const handleOnSubmit = async values => {
     await axios.post("/signUp", values);
     return navigate("/input");
   };
   return (
     <form onSubmit={form.onSubmit(handleOnSubmit)}>
-      <TextInput
-        withAsterisk
-        label="Email"
-        placeholder="your@email.com"
-        key={form.key("email")}
-        {...form.getInputProps("email")}
-      />
+      <Stack gap="lg" p="20px">
+        <TextInput
+          withAsterisk
+          label="Email"
+          placeholder="your@email.com"
+          key={form.key("email")}
+          {...form.getInputProps("email")}
+        />
 
-      <TextInput
-        withAsterisk
-        label="Name"
-        placeholder="ネーム"
-        key={form.key("name")}
-        {...form.getInputProps("name")}
-      />
+        <TextInput
+          withAsterisk
+          label="Name"
+          placeholder="ネーム"
+          key={form.key("name")}
+          {...form.getInputProps("name")}
+        />
 
-      <TextInput
-        withAsterisk
-        label="Password"
-        placeholder="パスワード"
-        key={form.key("password")}
-        {...form.getInputProps("password")}
-      />
+        <TextInput
+          withAsterisk
+          label="Password"
+          placeholder="パスワード"
+          key={form.key("password")}
+          {...form.getInputProps("password")}
+        />
 
-      <Group justify="flex-end" mt="md">
-        <Button type="submit">Submit</Button>
-      </Group>
+        <Group justify="flex-end" mt="md">
+          <Button type="submit">作成</Button>
+        </Group>
+      </Stack>
     </form>
   );
 };
